28b87a6893d03ea17a95b03be43180369050875d
ocavue <ocavue@gmail.com>
2022-06-21 15:14:45 +0800

    Add next.js example

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # remirror-examples
-Remirror examples
+
+- [Next.js](https://nextjs.org/) Example: [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remirror/remirror-examples/tree/main/with-nextjs)
---
+++ with-nextjs/components/editor.tsx
@@ -0,0 +1,48 @@
+import "remirror/styles/all.css";
+
+import { OnChangeJSON, Remirror, useRemirror } from "@remirror/react";
+import { useCallback, useState } from "react";
+import type { RemirrorJSON } from "remirror";
+import { BoldExtension } from "remirror/extensions";
+
+const STORAGE_KEY = "remirror-editor-content";
+
+const Editor: React.FC = () => {
+  const [initialContent] = useState<RemirrorJSON | undefined>(() => {
+    // Retrieve the JSON from localStorage (or undefined if not found)
+    const content = window.localStorage.getItem(STORAGE_KEY);
+    return content ? JSON.parse(content) : undefined;
+  });
+
+  const handleEditorChange = useCallback((json: RemirrorJSON) => {
+    // Store the JSON in localStorage
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(json));
+  }, []);
+
+  return (
+    <CoreEditor onChange={handleEditorChange} initialContent={initialContent} />
+  );
+};
+
+interface MyEditorProps {
+  onChange: (json: RemirrorJSON) => void;
+  initialContent?: RemirrorJSON;
+}
+
+const CoreEditor: React.FC<MyEditorProps> = ({ onChange, initialContent }) => {
+  const { manager } = useRemirror({ extensions: () => [new BoldExtension()] });
+  return (
+    <div style={{ width: "100%", maxWidth: "500px" }}>
+      <Remirror
+        placeholder="Enter text..."
+        manager={manager}
+        autoRender={true}
+        initialContent={initialContent}
+      >
+        <OnChangeJSON onChange={onChange} />
+      </Remirror>
+    </div>
+  );
+};
+
+export default Editor;
